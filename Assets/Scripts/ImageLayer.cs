7b3d0769ec668aab8d72b7de4384598200eb5b6e
KIBA <kjh030529@gmail.com>
2022-04-26 00:56:49 +0900

    Commit ImageLayer.cs

---
+++ Assets/Scripts/ImageLayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageLayer : Layer
+{
+    public Texture2D media;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        InitLayer(); // Layer Initialize Codes
+        InitScaler();
+        SetImage(media);
+    }
+
+    public void SetImage(Texture2D media)
+    {
+        this.media = media;
+
+        LayerImage.texture = media;
+        Debug.Log($"[VideoLayer] Size Ratio : ({media.width}, {media.height})");
+
+        ScalingToRatio(new Vector2(media.width, media.height));
+        
+    }
+}
